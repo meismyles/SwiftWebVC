90c401559cab2a667f6de336cad689b1cd1f0a29
Thanh-Nhon Nguyen <ntnhon.cs@gmail.com>
2016-07-23 20:03:50 +0200

    - Make enum for theme case. - Update example. - Add vietnamese localization.

--- SwiftWebVCExample/SwiftWebVCExample/ViewController.swift
+++ SwiftWebVCExample/SwiftWebVCExample/ViewController.swift
@@ -12,18 +12,23 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-    
-    let but = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 100))
-    but.setTitle("Open Google", forState: .Normal)
-    but.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
-    but.addTarget(self, action: #selector(ViewController.present), forControlEvents: .TouchUpInside)
-    view.addSubview(but)
   }
   
-  func present()
+  @IBAction func presentWithDefaultTheme()
   {
     let webVC = SwiftModalWebVC(urlString: "http://google.com")
+    self.presentViewController(webVC, animated: true, completion: nil)
+  }
+    
+  @IBAction func presentWithLightBlackTheme()
+  {
+    let webVC = SwiftModalWebVC(urlString: "http://google.com", theme: .lightBlack)
+    self.presentViewController(webVC, animated: true, completion: nil)
+  }
+    
+  @IBAction func presentWithDarkTheme()
+  {
+    let webVC = SwiftModalWebVC(urlString: "http://google.com", theme: .dark)
     self.presentViewController(webVC, animated: true, completion: nil)
   }
 
