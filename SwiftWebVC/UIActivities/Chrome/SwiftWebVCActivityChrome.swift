751a31489367a10f80f06022bb3b9901d70027b8
Eugene Antropov <antigp@gmail.com>
2016-09-19 17:48:19 +0300

    Update to Swift 3

--- SwiftWebVC/UIActivities/Chrome/SwiftWebVCActivityChrome.swift
+++ SwiftWebVC/UIActivities/Chrome/SwiftWebVCActivityChrome.swift
@@ -10,21 +10,21 @@ import UIKit
 
 class SwiftWebVCActivityChrome : SwiftWebVCActivity {
 
-    override func activityTitle() -> String {
+    override var activityTitle : String {
         return NSLocalizedString("Open in Chrome", tableName: "SwiftWebVC", comment: "")
     }
     
-    override func canPerformWithActivityItems(activityItems: [AnyObject]) -> Bool {
+    override func canPerform(withActivityItems activityItems: [Any]) -> Bool {
         for activityItem in activityItems {
-            if activityItem .isKindOfClass(NSURL) && UIApplication.sharedApplication().canOpenURL(NSURL(string: "googlechrome://")!) {
+            if activityItem is URL, UIApplication.shared.canOpenURL(URL(string: "googlechrome://")!) {
                 return true;
             }
         }
         return false;
     }
     
-    override func performActivity() {
-        let inputURL: NSURL! = URLToOpen
+    override func perform() {
+        let inputURL: URL! = URLToOpen as URL!
         let scheme: String! = inputURL.scheme
         
         // Replace the URL Scheme with the Chrome equivalent.
@@ -38,14 +38,14 @@ class SwiftWebVCActivityChrome : SwiftWebVCActivity {
         
         // Proceed only if a valid Google Chrome URI Scheme is available.
         if chromeScheme != nil {
-            let absoluteString: NSString! = inputURL!.absoluteString
-            let rangeForScheme: NSRange! = absoluteString.rangeOfString(":")
-            let urlNoScheme: String! = absoluteString.substringFromIndex(rangeForScheme.location)
+            let absoluteString: NSString! = inputURL!.absoluteString as NSString!
+            let rangeForScheme: NSRange! = absoluteString.range(of: ":")
+            let urlNoScheme: String! = absoluteString.substring(from: rangeForScheme.location)
             let chromeURLString: String! = chromeScheme!+urlNoScheme
-            let chromeURL: NSURL! = NSURL(string: chromeURLString)
+            let chromeURL: URL! = URL(string: chromeURLString)
             
             // Open the URL with Chrome.
-            UIApplication.sharedApplication().openURL(chromeURL)
+            UIApplication.shared.openURL(chromeURL)
         }
     }
     
