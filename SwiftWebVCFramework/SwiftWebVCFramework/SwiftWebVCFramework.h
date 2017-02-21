9c3afda6a47a67e0c23fa8f1b8fdc28f9eb0933e
Megan Williams <mwilliams@mwilliams.ats.ucla.edu>
2017-02-21 15:13:30 -0800

    Added Carthage Compatibility

---
+++ SwiftWebVCFramework/SwiftWebVCFramework/SwiftWebVCFramework.h
@@ -0,0 +1,19 @@
+//
+//  SwiftWebVCFramework.h
+//  SwiftWebVCFramework
+//
+//  Created by Megan Williams on 2/21/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftWebVCFramework.
+FOUNDATION_EXPORT double SwiftWebVCFrameworkVersionNumber;
+
+//! Project version string for SwiftWebVCFramework.
+FOUNDATION_EXPORT const unsigned char SwiftWebVCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftWebVCFramework/PublicHeader.h>
+
+
